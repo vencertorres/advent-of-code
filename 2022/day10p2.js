68fb340e221c5dfa5c3f3aa6a21068218f88e5c2
Vencer Torres <vencer.torres@gmail.com>
2022-12-13 20:23:46 +0800

    Day 10

---
+++ 2022/day10p2.js
@@ -0,0 +1,170 @@
+const input = `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`.split(/\n/);
+
+const selected_cycles = new Set([40, 80, 120, 160, 200, 240]);
+let x = 1;
+let cycle = 0;
+let sprite_position = [x - 1, x, x + 1];
+
+for (let instruction of input) {
+  const [_, V] = instruction.split(" ");
+  const cycles = V ? 3 : 2;
+  for (let i = 1; i < cycles; i++) {
+    if (selected_cycles.has(cycle)) {
+      console.log();
+    }
+
+    if (sprite_position.includes(cycle % 40)) {
+      process.stdout.write("#");
+    } else {
+      process.stdout.write(".");
+    }
+    cycle += 1;
+  }
+  x += V ? parseInt(V) : 0;
+  sprite_position = [x - 1, x, x + 1];
+}
